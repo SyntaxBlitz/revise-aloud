a981010633981ff98564c655a1f05dc18ffd2c66
Timothy Aveni <timothy@tacosareawesome.com>
2014-07-11 11:26:38 -0400

    Split paragraphs with delimiter

--- index.js
+++ index.js
@@ -4,9 +4,31 @@ window.onload = function () {
 };
 
 var startProcess = function () {
+	var paragraphs = getParagraphArray();
+	initialiseRecordingInterface();
+
+	showRenderView();
+};
+
+var initialiseRecordingInterface = function () {
+
+};
+
+var showRenderView = function () {
 	var enterText = document.getElementById("enterText");
 	enterText.style.display = "none";
 
 	var renderText = document.getElementById("renderText");
 	renderText.style.display = "block";
+};
+
+var getParagraphArray = function () {
+	var textarea = document.getElementById("enterField");
+	var delimiterInput = document.getElementById("paragraphDelimiterInput");
+	
+	var contents = textarea.value;
+	var delimiter = (delimiterInput.value === "")?
+						"\n\n":
+						delimiterInput.value;
+	return contents.split(new RegExp(delimiter));
 };
